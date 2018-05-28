bf5df3ceed8912fd19fc116c91132a691709d628
Darren-Ivey <divey@mylifedigital.co.uk>
2018-05-28 14:18:18 +0100

    Update stylings

--- src/components/LocationAndDateForm/LocationAndDateForm.js
+++ src/components/LocationAndDateForm/LocationAndDateForm.js
@@ -10,20 +10,22 @@ export default class LocationAndDateForm extends Component {
 
     render () {
         return (
-            <View>
+            <View style={styles.form}>
                 <Text style={styles.formHeader}>Search for your sunrise and sunset times</Text>
                 <View style={styles.field}>
-                    <Text style={styles.fieldLabel}>Postcode</Text>
-                    <TextInput style={styles.fieldInput} />
+                    <Text>Postcode</Text>
+                    <TextInput maxLength={8} style={styles.fieldInput} />
                 </View>
                 <View style={styles.field}>
-                    <Text style={styles.fieldLabel}>Date</Text>
+                    <Text>Date</Text>
                     <TextInput style={styles.fieldInput} />
                 </View>
-                <View style={styles.footer}>
-                    <Button onPress={this.handleSubmit} style={styles.footerButton} title="Submit form">
-                        Find
-                    </Button>
+                <View>
+                    <View style={styles.footerButton}>
+                        <Text color="#841584" onPress={this.handleSubmit} style={styles.footerButtonText} title="Submit form">
+                            Find
+                        </Text>
+                    </View>
                 </View>
             </View>
         )
@@ -31,25 +33,43 @@ export default class LocationAndDateForm extends Component {
 }
 
 const styles = StyleSheet.create({
+    form: {
+        borderTopWidth: 2,
+        borderColor: '#ffffff',
+    },
     formHeader: {
         color: '#333',
         marginTop: 24,
         marginBottom: 16,
-        fontSize: 14,
-    },
-    fieldLabel: {
-    },
-    fieldInputText: {
+        fontSize: 16,
     },
     footerButton: {
+        paddingTop: 8,
+        paddingBottom: 8,
+        paddingRight: 16,
+        paddingLeft: 16,
+        borderRadius: 4,
+        backgroundColor: '#61b2a7',
+        width: 'auto',
+        alignSelf: 'flex-start'
     },
-    footer: {
+    footerButtonText: {
+        fontWeight: '400',
+        fontSize: 14,
+        color: 'white',
+        alignSelf: 'center'
     },
     field: {
         marginBottom: 16
     },
     fieldInput: {
         borderWidth: 1,
-        borderColor: 'black',
+        borderColor: '#d9d9d9',
+        padding: 4,
+        marginTop: 8,
+        marginBottom: 8,
+        borderRadius: 2,
+        fontSize: 12,
+        maxWidth: 320
     }
 });
--- src/pages/SunActivityPage.js
+++ src/pages/SunActivityPage.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     },
     pageSunActivityHeader: {
         color: '#333',
-        fontWeight: '300',
+        fontSize: 20,
         paddingBottom: 8,
         marginTop: 24,
     }
